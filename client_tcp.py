610c78c1764adc564575ad47c4e4bc0d999be0c9
Akira Watanabe <a0323w@gmail.com>
2023-06-24 17:36:04 +0900

    ファイル名変更

--- client_tcp.py
+++ client_tcp.py
@@ -1,27 +1,40 @@
-'''クライアント'''
+'''送信側(TCP)'''
 import socket
+import os
 import time
+import keyboard
 
 HOST = '127.0.0.1'
-PORT = 50007
+MAINPORT = 50007
 
-def data_client():
-    '''クライアントの働きをする'''
-    #接続
+def connect_unity():
+    '''Unityにデータを送る'''
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-    print('Connected to server. IP:', HOST, 'Port:', PORT)
 
-    #送信
-    data_to_send = str(time.time_ns())
-    client.sendall(data_to_send.encode('utf-8'))
+    result = str(os.getpid())
+    print(os.getpid())
 
-    #応答を受け取る
-    received_data = client.recv(1024).decode('utf-8')
-    print('Received data:', received_data)
+    #Unityに接続
+    client.connect((HOST, MAINPORT))
 
-    client.close()
+    #データを送信する
+    client.send(result.encode('utf-8'))
+    #client.send(str(time.time_ns()).encode('utf-8'))
+
+    print(time.time_ns())
+
+    #データを受信する
+    #1024 ... 受信するデータのバイト数（この数値以下のバイト数のデータなら、問題なく実行される）
+    data = client.recv(1024)
+
+    print(data.decode('utf-8'))
+
+    if keyboard.is_pressed('s'):
+        client.close()
+
     time.sleep(2.0)
 
+    #return client
+
 while True:
-    data_client()
+    connect_unity()
